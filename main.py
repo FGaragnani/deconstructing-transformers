7a11180f662c7b4741188dfd34773d37c48ee8a0
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-07-31 01:00:45 +0200

    Set random seed

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+import torch
 from src.train import train_model
 from src.model import TransformerLikeModel
 from dataset.dataset import parse_dataset_from_xls, SheetType, PreprocessingTimeSeries
 
 from torch.utils.data import DataLoader
 
+OUTPUT_LEN = 4
+
 def main():
-    train_dataset, test_dataset = parse_dataset_from_xls("M3C.xls", SheetType.YEARLY, row=227, output_len=6, preprocessing=PreprocessingTimeSeries.NORMALIZE)
+    torch.manual_seed(42)
+
+    model: TransformerLikeModel = TransformerLikeModel(embed_size=24, encoder_size=1, decoder_size=0, output_len=OUTPUT_LEN, num_head_enc=2)
+    train_dataset, test_dataset = parse_dataset_from_xls("M3C.xls", SheetType.YEARLY, row=227, output_len=OUTPUT_LEN, preprocessing=PreprocessingTimeSeries.NORMALIZE)
     train_loader, test_loader = DataLoader(train_dataset, batch_size=1, shuffle=True), DataLoader(test_dataset, batch_size=1, shuffle=False)
-    model: TransformerLikeModel = TransformerLikeModel(embed_size=32, encoder_size=1, decoder_size=2, output_len=6)
-    train_model(model=model, epochs=20, train_data_loader=train_loader, test_data_loader=test_loader)
+    train_model(model=model, epochs=25, train_data_loader=train_loader, test_data_loader=test_loader)
 
 if __name__ == "__main__":
     main()
