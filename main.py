60c18e95f99fb7d4db103b0f343ef71698afc834
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-07-31 22:33:53 +0200

    Updated training loop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+from src.seca import ScalarExpansionContractiveAutoencoder
 from src.train import train_transformer_model
 from src.model import TransformerLikeModel
 from dataset.dataset import parse_whole_dataset_from_xls, SheetType, PreprocessingTimeSeries, DatasetTimeSeries
@@ -6,7 +7,7 @@ from dataset.dataset import parse_whole_dataset_from_xls, SheetType, Preprocessi
 from torch.utils.data import DataLoader
 from typing import List, Tuple
 
-OUTPUT_LEN = 1
+OUTPUT_LEN = 4
 
 def main():
     torch.manual_seed(42)
@@ -16,13 +17,15 @@ def main():
     best_dataset: Tuple[DatasetTimeSeries, DatasetTimeSeries] = datasets[0]
     best_train_loss, best_test_loss = float('inf'), float('inf')
 
+    seca = ScalarExpansionContractiveAutoencoder(embed_size=8, input_size=1) # Preload SECA so it avoids pretraining in each iteration
+
     for idx, (train_dataset, test_dataset) in enumerate(datasets):
         print(f"Training on dataset: {train_dataset.category} (ID: {train_dataset.id})")
         print(f"Number of training samples: {len(train_dataset)}, Number of testing samples: {len(test_dataset)}")
     
-        model: TransformerLikeModel = TransformerLikeModel(embed_size=8, encoder_size=1, decoder_size=1, output_len=OUTPUT_LEN, num_head_enc=2, positional_embedding_method="learnable")
+        model: TransformerLikeModel = TransformerLikeModel(embed_size=8, encoder_size=1, decoder_size=1, output_len=OUTPUT_LEN, num_head_enc=2, positional_embedding_method="learnable", seca=seca)
         train_loader, test_loader = DataLoader(train_dataset, batch_size=1, shuffle=True), DataLoader(test_dataset, batch_size=1, shuffle=False)
-        train_loss, test_loss = train_transformer_model(model=model, epochs=30, train_data_loader=train_loader, test_data_loader=test_loader, verbose=False)
+        train_loss, test_loss = train_transformer_model(model=model, epochs=30, train_data_loader=train_loader, test_data_loader=test_loader, verbose=False, pretrain_seca=(idx == 0))
         if test_loss < best_test_loss:
             best_train_loss = train_loss
             best_test_loss = test_loss
