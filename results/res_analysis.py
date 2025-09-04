9a2677ccf8a50dd3d3be6d639095f38debb8bc2f
Vittorio Maniezzo <vittorio.maniezzo@unibo.it>
2025-09-04 21:23:26 +0200

    sept4

---
+++ results/res_analysis.py
@@ -0,0 +1,26 @@
+import numpy as np, pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import mannwhitneyu
+
+
+def main():
+   df = pd.read_csv("res_monthly.csv")
+   unique_types = df['type'].unique()
+   countTrain = (df['tr_train_RMSE'] < df['rf_train_RMSE']).sum()
+   countTest  = (df['tr_test_RMSE'] < df['rf_test_RMSE']).sum()
+   print(f"n={len(df)} train {countTrain} test {countTest}")
+   
+   for type in unique_types:
+      dfType = df[df.type==type]
+      countTrain = (dfType['tr_train_RMSE'] < dfType['rf_train_RMSE']).sum()
+      countTest = (dfType['tr_test_RMSE'] < dfType['rf_test_RMSE']).sum()
+      
+      dfTr = dfType["tr_test_RMSE"]
+      dfRF = dfType["rf_test_RMSE"]
+      statistic, p_value = mannwhitneyu(dfTr, dfRF, alternative='two-sided')
+      
+      print(f"Type {type} num {len(dfType)}  train {countTrain} test {countTest} ({(100*countTest/len(dfType)):.2f} p={p_value:.3f})")
+   return
+
+if __name__ == "__main__":
+   main()
