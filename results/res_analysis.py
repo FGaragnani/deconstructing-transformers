40ef82101ff19d2887d0f6ddfd04ff97cbe58e01
Vittorio Maniezzo <vittorio.maniezzo@unibo.it>
2025-09-05 10:44:19 +0200

    sapt5

--- results/res_analysis.py
+++ results/res_analysis.py
@@ -10,16 +10,20 @@ def main():
    countTest  = (df['tr_test_RMSE'] < df['rf_test_RMSE']).sum()
    print(f"n={len(df)} train {countTrain} test {countTest}")
    
+   dfM3 = pd.read_excel("../M3C.xls", sheet_name="M3Month")
+   
    for type in unique_types:
       dfType = df[df.type==type]
       countTrain = (dfType['tr_train_RMSE'] < dfType['rf_train_RMSE']).sum()
       countTest = (dfType['tr_test_RMSE'] < dfType['rf_test_RMSE']).sum()
+      notNA = dfM3[dfM3.Category==type].notna().iloc[:,6:]
+      avgLength = notNA.sum().sum()/len(notNA)
       
       dfTr = dfType["tr_test_RMSE"]
       dfRF = dfType["rf_test_RMSE"]
       statistic, p_value = mannwhitneyu(dfTr, dfRF, alternative='two-sided')
       
-      print(f"Type {type} num {len(dfType)}  train {countTrain} test {countTest} ({(100*countTest/len(dfType)):.2f} p={p_value:.3f})")
+      print(f"Type {type} num {len(dfType)} len {avgLength:.2f} train {countTrain} test {countTest} ({(100*countTest/len(dfType)):.2f} p={p_value:.3f})")
    return
 
 if __name__ == "__main__":
