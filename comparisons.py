06f7a7091a99a54c568be55605237b086db6e73c
Filippo Garagnani <101595335+FGaragnani@users.noreply.github.com>
2025-09-26 15:31:36 +0200

    First try on TCAN

--- comparisons.py
+++ comparisons.py
@@ -4,11 +4,19 @@ import torch
 import pandas as pd
 from typing import List, Tuple
 from dataset.dataset import parse_whole_dataset_from_xls, SheetType, PreprocessingTimeSeries, DatasetTimeSeries
+
 from chronos import BaseChronosPipeline
+from tcan.TCAN.model import Transformer
+from tcan.TCAN.utils import Params
+from tcan.TCAN.model.Models import loss_fn
+
 from tqdm import tqdm
 
 def main():
     torch.manual_seed(42)
+
+    CHRONOS = False
+    TCAN = True
 
     OUTPUT_LEN = 18
     INPUT_LEN = 24
@@ -27,34 +35,66 @@ def main():
 
     datasets: List[Tuple[DatasetTimeSeries, DatasetTimeSeries]] = parse_whole_dataset_from_xls("M3C.xls", SheetType.MONTHLY, input_len=INPUT_LEN, output_len=OUTPUT_LEN, preprocessing=PreprocessingTimeSeries.MIN_MAX)
     datasets = [dataset for dataset in datasets if dataset[0].id in indices]
-
-    chronos_model = BaseChronosPipeline.from_pretrained(
-        "amazon/chronos-t5-small",  # use "amazon/chronos-bolt-small" for the corresponding Chronos-Bolt model
-        device_map="cpu",  # use "cpu" for CPU inference
-        torch_dtype=torch.bfloat16,
-    )
-
+    
     results = []
-    for i, (train_ds, test_ds) in enumerate(tqdm(datasets, desc="Evaluating datasets")):
-        input_series = torch.tensor(train_ds.original_series, dtype=torch.float32).unsqueeze(0)  # shape (1, seq_len)
 
-        with torch.no_grad():
-            forecast = chronos_model.predict(input_series, prediction_length=len(test_ds.original_series)) # forecast shape: (1, prediction_length)
+    if CHRONOS:
+        chronos_model = BaseChronosPipeline.from_pretrained(
+            "amazon/chronos-t5-small",
+            device_map="cpu",  
+            torch_dtype=torch.bfloat16,
+        )
 
-        forecast = forecast.squeeze(0).cpu().numpy()
-        target = test_ds.original_series
+        for i, (train_ds, test_ds) in enumerate(tqdm(datasets, desc="Evaluating datasets")):
+            input_series = torch.tensor(train_ds.original_series, dtype=torch.float32).unsqueeze(0)  # shape (1, seq_len)
 
-        rmse = (((forecast - target) ** 2).mean()) ** 0.5
+            with torch.no_grad():
+                forecast = chronos_model.predict(input_series, prediction_length=len(test_ds.original_series)) # forecast shape: (1, prediction_length)
 
-        results.append({
-            "dataset_id": train_ds.id,
-            "rmse": rmse,
-            "forecast": forecast,
-            "target": target,
-        })
+            forecast = forecast.squeeze(0).cpu().numpy()
+            target = test_ds.original_series
 
-    for r in results:
-        print(f"Dataset {r['dataset_id']}: RMSE={r['rmse']:.4f}")
+            rmse = (((forecast - target) ** 2).mean()) ** 0.5
+
+            results.append({
+                "dataset_id": train_ds.id,
+                "rmse": rmse,
+                "forecast": forecast,
+                "target": target,
+            })
+
+        print("Chronos Model")
+        print("==============")
+        for r in results:
+            print(f"Dataset {r['dataset_id']}: RMSE={r['rmse']:.4f}")
+
+    if TCAN:
+        params = Params('tcan/TCAN/experiments/params_M3C.json')
+        params.dict["train_window"] = INPUT_LEN
+        params.dict["test_window"] = OUTPUT_LEN
+        params.dict["predict_start"] = INPUT_LEN - 1
+        params.dict["num_epochs"] = EPOCHS
+        params.dict["batch_size"] = BATCH_SIZE
+        params.dict["predict_steps"] = 6
+
+        dataset = "M3C"
+
+        tcan_model = Transformer(params, dataset)
+        tcan_model.train()
+
+        optimizer = torch.optim.Adam(tcan_model.parameters(), lr=1e-3)
+        for epoch in range(EPOCHS):
+            for train_ds, _ in datasets:
+                train_loader = torch.utils.data.DataLoader(train_ds.tensor_dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True)
+                for X_batch, Y_batch in train_loader:
+                    X_batch, Y_batch = X_batch.unsqueeze(-1), Y_batch.unsqueeze(-1) # shape (batch_size, seq_len, feature_dim)
+                    optimizer.zero_grad()
+                    mu, sigma = tcan_model(X_batch, Y_batch)
+                    loss = loss_fn(mu, sigma, Y_batch, params.dict["predict_start"])
+                    loss.backward()
+                    optimizer.step()
+
+
 
 if __name__ == "__main__":
     main()
